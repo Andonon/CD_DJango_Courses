6262bea861746e53cb13adf57b2ca5a8114041fa
Troy Center <troycenter1@gmail.com>
2017-06-23 08:14:44 -0700

    moving back to master branch

--- apps/coursesapp/views.py
+++ apps/coursesapp/views.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 #pylint: disable=E1101
 from __future__ import unicode_literals
-
 from django.shortcuts import render, redirect
 from .models import Courses
+
 # Create your views here.
 def index(request):
     '''home page, has courses list on it'''
-    print "1"*50
+    print "home*"*20
     print Courses.objects.all()
     # print Courses.objects.all().delete()
-    print "1"*50
     context = {
         "courses": Courses.objects.all()
     }
@@ -18,11 +17,25 @@ def index(request):
 
 def addcourse(request):
     ''' add course route, for processing db request '''
-    print "8"*50
+    print "add*"*20
     print request.POST
-    print "8"*50
     Courses.objects.create(
         course_name=request.POST['course_name'],
-        description=request.POST['course_description']
+        description=request.POST['description']
     )
+    return redirect('/')
+
+def destroy(request, course_id):
+    ''' destroy confirm page, confirm with user '''
+    print "Destroy*"*20
+    print Courses.objects.all()
+    print "Destroy*"*20
+    itemtodelete = Courses.objects.get(id=course_id)
+    print itemtodelete
+    return redirect('/')
+
+def confirmdestroy(request, course_id):
+    ''' destroy confirm page, confirm with user '''
+    print "ConfirmDestroy*"*20
+    print request.POST
     return redirect('/')
